29ec1444aa6264dea9a807de4d197dbefb296242
Chamuy123 <yathamchamundeswari@gmail.com>
2023-12-04 21:30:43 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def chamu(request):
+    return render(request,'chamu.html')
+
+def manasa(request):
+    return render (request,'manasa.html')
+
+def chamu2(request):
+    return render(request,'chamu2.html')
+
